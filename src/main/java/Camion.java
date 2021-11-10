ac7839db285b19093fcaeac8850e6ff2b96368b0
j-ortiz-ici <j.ortiz07@ufromail.cl>
2021-11-10 09:33:18 -0300

    2 commit

--- src/main/java/Camion.java
+++ src/main/java/Camion.java
@@ -8,6 +8,15 @@ public class Camion {
 	public Sucursal _sucursal;
 	public ArrayList<Flete> _fletes = new ArrayList<Flete>();
 
+	public Camion(int codigoCamion, int patente, String descripcion, boolean estado, Sucursal _sucursal, ArrayList<Flete> _fletes) {
+		this.codigoCamion = codigoCamion;
+		this.patente = patente;
+		this.descripcion = descripcion;
+		this.estado = estado;
+		this._sucursal = _sucursal;
+		this._fletes = _fletes;
+	}
+
 	public void cambiarEstado() {
 		throw new UnsupportedOperationException();
 	}
--- src/main/java/Sucursal.java
+++ src/main/java/Sucursal.java
@@ -5,4 +5,16 @@ public class Sucursal {
 	private String region;
 	public Empresa _empresa;
 	public ArrayList<Camion> _camion = new ArrayList<Camion>();
+
+	public Sucursal(int codigoSucursal, String region, Empresa _empresa, ArrayList<Camion> _camion) {
+		this.codigoSucursal = codigoSucursal;
+		this.region = region;
+		this._empresa = _empresa;
+		this._camion = _camion;
+	}
+	public void agregarCamion(int codigoCamion,int patente, String descripcion,
+							  boolean estado,Sucursal sucursal, ArrayList<Flete> fletes) {
+		Camion camion = new Camion(codigoCamion,patente,descripcion,estado,sucursal,fletes);
+		this._camion.add(camion);
+	}
 }
