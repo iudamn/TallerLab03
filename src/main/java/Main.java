a86a00fc521b281794686deb5cb83556cd9b9be2
tonis <c.cartes07@ufromail.cl>
2021-11-10 10:34:03 -0300

    commit

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -3,6 +3,7 @@ import java.io.IOException;
 public class Main {
     public static void main(String[] args) throws IOException {
         Menu menu1= new Menu();
+        Flete flete1= new Flete();
         menu1.menu();
     }
 }
